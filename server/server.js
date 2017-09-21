324b6e4dd4270b4b4359f68755817147157925a2
Jay Hongvarivatana <jayyhong@gmail.com>
2017-09-21 11:41:15 -0700

    added a get to server

--- server/server.js
+++ server/server.js
@@ -6,13 +6,17 @@ const path = require('path');
 
 const app = express()
   .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended:true }))
+  .use(bodyParser.urlencoded({ extended: true }))
   .use(express.static(path.resolve(__dirname, '../dist')))
   .use(morgan('dev'))
 
-  app.listen(PORT, err=>{
-    if (err) {
-      console.log(err)
-    }
-    console.log('Server successfully connect on ' + PORT)
-  })
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.listen(PORT, err => {
+  if (err) {
+    console.log(err)
+  }
+  console.log('Server successfully connect on ' + PORT)
+})
