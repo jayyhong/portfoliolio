60e14fb5a21f9aab6957f40c8eb7e36e694c61f1
Jay Hongvarivatana <jayyhong@gmail.com>
2017-11-02 15:32:56 -0700

    Added level for express compression, does not seem to be doing anything

--- server/server.js
+++ server/server.js
@@ -1,41 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3300;
-const path = require('path');
-const compression = require('compression');
+const path = require("path");
+const compression = require("compression");
 
 const app = express()
+  .use(compression({level: 9}))
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
-  .use(express.static(path.resolve(__dirname, '../dist')))
-  .use(morgan('dev'))
-
-app.use(compression());
-app.get('/events', function (req, res) {
-  res.setHeader('Content-Type', 'text/event-stream')
-  res.setHeader('Cache-Control', 'no-cache')
-
-  // send a ping approx every 2 seconds
-  var timer = setInterval(function () {
-    res.write('data: ping\n\n')
-
-    // !!! this is the important part
-    res.flush()
-  }, 2000)
-
-  res.on('close', function () {
-    clearInterval(timer)
-  })
-})
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
-})
-
+  .use(express.static(path.resolve(__dirname, "../dist")));
+  
 app.listen(PORT, err => {
   if (err) {
-    console.log(err)
+    console.log(err);
   }
-  console.log('Server successfully connect on ' + PORT)
-})
+  console.log("Server successfully connect on " + PORT);
+});
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
