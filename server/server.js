7b3f829a35fae41e9664e15593fdb3423c2999ef
Jay Hongvarivatana <jayyhong@gmail.com>
2017-11-02 15:43:38 -0700

    working with compression

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
+const compression = require("compression");
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3300;
-const path = require("path");
-const compression = require("compression");
+const app = express();
 
-const app = express()
-  .use(compression({level: 9}))
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
-  .use(express.static(path.resolve(__dirname, "../dist")));
-  
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "../dist")));
+
 app.listen(PORT, err => {
   if (err) {
     console.log(err);
